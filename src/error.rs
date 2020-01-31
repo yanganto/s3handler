f04cd8739d40bc3b09079020a8aec9fb813e601c
yanganto <yanganto@gmail.com>
2020-01-31 12:57:00 +0800

    Add failure description

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,17 @@
 //! Error management module
+use failure_derive::*;
 
-#[derive(Debug)]
+#[derive(Fail, Debug)]
 pub enum Error {
+    #[fail(display = "Could not load file: {}", 0)]
     LoadError(std::io::Error),
+    #[fail(display = "The response should be JSON: {}", 0)]
     JSONParseError(serde_json::Error),
+    #[fail(display = "The response should be XML: {}", 0)]
     XMLParseError(quick_xml::Error),
+    #[fail(display = "Unexpected input from user: {}", 0)]
     UserError(&'static str),
+    #[fail(display = "Can not make a request")]
     ReqwestError(),
 }
 
